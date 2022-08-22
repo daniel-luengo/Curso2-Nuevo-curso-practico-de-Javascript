bcd8cd8c437d3f56f53ef9b3956abbf24b3540c2
daniel-luengo <dluengo@datview.cl>
2022-08-22 17:54:29 -0400

    Un ejemplo de SWITCH

---
+++ switch.js
@@ -0,0 +1,21 @@
+
+
+
+const tipoSuscripcion = "Basic";
+
+
+
+switch (tipoSuscripcion) {
+    case  "Free":
+        console.log("Tienes la suscripcion Free");
+        break
+    case "Basic":
+        console.log("Tienes la suscripcion basica")
+        break;
+    case "Expert":
+        console.log("Tienes la suscripcion expert");
+        break;
+    case "ExpertDuos":
+        console.log("Tienes la suscripcion para dos personas")
+        break
+}
