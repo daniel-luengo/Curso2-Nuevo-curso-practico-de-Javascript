15292997306faa7b2c161386abf4fb963431a3af
daniel-luengo <dluengo@datview.cl>
2022-08-22 16:54:18 -0400

    Repaso prueba de funciones aunque no esta anotada la mayoria

---
+++ Funciones.js
@@ -0,0 +1,17 @@
+let nombre;
+let apellido;
+
+function nombreCompleto (nombre , apellido) {
+    return nombre + " " + apellido;
+}
+
+nombreCompleto("Daniel", "Broom");
+
+
+
+
+function nombreNickname(nombre, apellido, nickname) {
+    return ("Minombre es " + nombre + " " + apellido + " pero prefiero que me llames " + nickname);
+}
+
+nombreNickname("Daniel " + " Luengo" + " Kaneki")
