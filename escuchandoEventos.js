23b560b0adc32f6124ef7ac6cef189b979a8c77a
daniel-luengo <dluengo@datview.cl>
2022-09-01 11:06:41 -0400

    asas

---
+++ escuchandoEventos.js
@@ -0,0 +1,8 @@
+
+const input1 = document.querySelector ("#input-1")
+const input2 = document.querySelector("#input-2")
+
+function suma () {
+    resultado =  parseInt( input1.value) + parseInt(input2.value) 
+    console.log(resultado)
+}
